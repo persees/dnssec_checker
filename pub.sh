fd0099056bd32d235e702337a62a2dd6cbe35384
tjventura <tiago.ventura12@gmail.com>
2021-05-13 09:42:03 +0100

    added DNSSEC final validation

--- pub.sh
+++ pub.sh
@@ -4,7 +4,7 @@ mapfile -t pubs < list.txt
 
 
 current=0
-echo "servico,flag AD,status,DS,DNSKEY,RRSIG" > results.csv
+echo "servico,flag AD,status,DS,DNSKEY,RRSIG,DNSSEC validation" > results.csv
 
 while [ $current -lt $len ]
 do
@@ -14,14 +14,17 @@ do
 	ds_results=$(dig DS ${pubs[${current}]} +short)
 	dnskey_result=$(dig DNSKEY ${pubs[${current}]} +short)
 	rrsig_result=$(dig ${pubs[${current}]} +dnssec +short)
+	dig . DNSKEY | grep -Ev '^($|;)' > root_keys
+	dnssec_verified=$(dig ${pubs[${current}]} +sigchase +trusted-key=./root_keys | grep -o "DNSSEC validation is ok: SUCCESS")
 
 	echo ${flag_result}
 	echo ${status_result}
 	echo ${ds_results}
 	echo ${dnskey_result}
 	echo ${rrsig_result}
+	echo ${dnssec_verified}
 
-	echo ${pubs[${current}]},${flag_result},${status_result},${ds_results},${dnskey_result},${rrsig_result} >> results.csv
+	echo ${pubs[${current}]},${flag_result},${status_result},${ds_results},${dnskey_result},${rrsig_result},${dnssec_verified} >> results.csv
 	current=$(( $current + 1 ))
 	done
 
